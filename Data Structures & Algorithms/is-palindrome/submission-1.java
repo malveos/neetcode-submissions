9b9fdc4b80bc60388758aa73bb191e7fa7d9d6d3
malveos <malveos123@gmail.com>
2026-04-17 23:27:51 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder rev = new StringBuilder();
+        StringBuilder ori = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            if(Character.isLetterOrDigit(c)) {
+                rev.insert(0,Character.toLowerCase(c));
+                ori.append(Character.toLowerCase(c));
+            }
+        }
+        return rev.toString().equals(ori.toString());
+    }
+}
