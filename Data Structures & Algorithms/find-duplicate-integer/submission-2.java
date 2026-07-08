11bf581fb1b93804b5b1d0de804d06d1808d35c3
malveos <malveos123@gmail.com>
2026-07-08 10:36:35 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // mark in the number aray itself if it is present
+        for (int n : nums) {
+            int idx = Math.abs(n) - 1;
+            if (nums[idx] < 0) {
+                return Math.abs(n);
+            }
+            nums[idx] *= -1;
+        }
+        return 0;
+    }
+}
